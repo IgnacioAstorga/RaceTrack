dad63aa9c29a38465eba3d335014c22b1993c943
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-10-04 01:00:30 +0200

    Shape 2D
    * Created a Shape 2D asset. This asset defines a shape similar to a
    mesh, but on 2D rather than 3D.

---
+++ Assets/Code/Assets/Shape2D.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu()]
+public class Shape2D : ScriptableObject {
+
+	public Vector2[] points;
+	public Vector2[] normals;
+	public float[] us;
+	public int[] lines;
+}
