fd1c0d300c719004f34861df0a0affe2e02f9295
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-10-15 12:03:49 +0200

    Hover
    * Now the vehicle hovers over the ground. The CharacterController has
    been replaced with a Rigidbody component.

--- Assets/Code/Race/Vehicle/VehicleController.cs
+++ Assets/Code/Race/Vehicle/VehicleController.cs
@@ -1,15 +1,14 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody))]
 public class VehicleController : MonoBehaviour {
-
-	public Vector3 Velocity { get; private set; }
-
-	public Transform body;
 
 	public LayerMask raceTrackLayer;
 	public float gravityRayMaxDistance = 10f;
 	public float gravityReorientationSpeed = 360f;
+
+	public float hoverDistance = 1f;
+	public float hoverForce = 50f;
 
 	public float turnRate = 45f;
 	public float acceleration = 10f;
@@ -22,12 +21,14 @@ public class VehicleController : MonoBehaviour {
 	
 	private Vector3 _gravity;
 
-	private CharacterController _characterController;
+	private Rigidbody _rigidbody;
+	private Transform _transform;
 
 	void Awake() {
 
 		// Retrieves the desired componets
-		_characterController = GetComponent<CharacterController>();
+		_rigidbody = GetComponent<Rigidbody>();
+		_transform = transform;
 	}
 
 	void Update() {
@@ -58,16 +59,25 @@ public class VehicleController : MonoBehaviour {
 
 		// Accelerates the vehicle towards it's forward direction
 		Accelerate();
-
-		// Finally, moves the vehicle using it's velocity
-		Move();
 	}
 
 	private void MatchTrackCurvature() {
 
+		// Calculates the gravity from the track's curvature
+		CalculateGravity();
+
+		// Orientates the vehicle to match the gravity
+		OrientateToGravity();
+
+		// Makes the vehicle hover
+		HoverOverTrack();
+	}
+
+	private void CalculateGravity() {
+
 		// Casts a ray downwards to check for the track orientation
 		RaycastHit trackHit;
-		if (Physics.Raycast(body.position, -body.up, out trackHit, gravityRayMaxDistance, raceTrackLayer)) {
+		if (Physics.Raycast(_transform.position, -_transform.up, out trackHit, gravityRayMaxDistance, raceTrackLayer)) {
 
 			// Uses the track orientation for the gravity
 			float gravityMagnitude = Physics.gravity.magnitude;
@@ -78,13 +88,28 @@ public class VehicleController : MonoBehaviour {
 			// If the track was not hit, uses the default gravity
 			_gravity = Physics.gravity;
 		}
+	}
 
-		// Orientates the object to match the gravity
-		float normalAngle = Vector3.Angle(body.up, -_gravity);
-		Vector3 normalAxis = Vector3.Cross(body.up, -_gravity);
+	private void OrientateToGravity() {
+
+		// Orientates the vehicle to match the gravity
+		float normalAngle = Vector3.Angle(_transform.up, -_gravity);
+		Vector3 normalAxis = Vector3.Cross(_transform.up, -_gravity);
 		if (normalAngle > 0) {
-			Quaternion targetRotation = Quaternion.AngleAxis(normalAngle, normalAxis) * body.rotation;
-			body.rotation = Quaternion.RotateTowards(body.rotation, targetRotation, gravityReorientationSpeed * Time.deltaTime);
+			Quaternion targetRotation = Quaternion.AngleAxis(normalAngle, normalAxis) * _transform.rotation;
+			_transform.rotation = Quaternion.RotateTowards(_transform.rotation, targetRotation, gravityReorientationSpeed * Time.deltaTime);
+		}
+	}
+
+	private void HoverOverTrack() {
+
+		// Adds force to the vehicle to separate it from the track
+		RaycastHit trackHit;
+		if (Physics.Raycast(_transform.position, -_transform.up, out trackHit, hoverDistance, raceTrackLayer)) {
+
+			// The amount of force added is proportional to how close the vehicle is to the ground
+			float proportionalDistance = 1f - trackHit.distance / hoverDistance;
+			_rigidbody.AddForce(_transform.up * proportionalDistance * hoverForce);
 		}
 	}
 
@@ -92,42 +117,22 @@ public class VehicleController : MonoBehaviour {
 
 		// Rotates the forward direction using the turn rate
 		float amountTurned = turnRate * _horizontalInput * Time.deltaTime;
-		amountTurned *= (1 - Velocity.magnitude / maxSpeed);
-		Quaternion turnRotation = Quaternion.AngleAxis(amountTurned, body.up);
-		body.rotation = turnRotation * body.rotation;
-		Velocity = turnRotation * Velocity;
+		amountTurned *= (1 - _rigidbody.velocity.magnitude / maxSpeed);
+		Quaternion turnRotation = Quaternion.AngleAxis(amountTurned, _transform.up);
+		_transform.rotation = turnRotation * _transform.rotation;
+		_rigidbody.velocity = turnRotation * _rigidbody.velocity;
 	}
 
 	private void Accelerate() {
 
 		// Accelerates the velocity using the vehicle's forward direction and user's input
-		Velocity += _accelerateInput * acceleration * body.forward * Time.deltaTime;
+		_rigidbody.velocity += _accelerateInput * acceleration * _transform.forward * Time.deltaTime;
 
 		// Adds the gravity to the velocity
-		Velocity += _gravity * Time.deltaTime;
+		_rigidbody.velocity += _gravity * Time.deltaTime;
 
 		// Applies drag to the velocity
 		float drag = Acceleration.GetDragFromAcceleration(acceleration, maxSpeed);
-		Velocity *= Mathf.Clamp01(1f - (1f + _brakeInput * brakeStrength) * drag * Time.fixedDeltaTime);
-	}
-
-	private void Move() {
-
-		// Moves the vehicle using the character's controlelr
-		_characterController.Move(Velocity * Time.deltaTime);
-	}
-
-	void OnControllerColliderHit(ControllerColliderHit hit) {
-
-		// In order to get a smooth normal, we need more information
-		Vector3 normal = hit.normal;
-		RaycastHit colliderHit;
-		Debug.DrawRay(hit.point + hit.normal, -hit.normal * 2 * hit.moveLength, Color.blue);
-		if (hit.collider.Raycast(new Ray(hit.point + hit.normal, -hit.normal), out colliderHit, 2 * hit.moveLength)) {
-			normal = colliderHit.SmoothedNormal();
-		}
-
-		// Projects the velocity along the hit surface
-		Velocity = Vector3.ProjectOnPlane(Velocity, normal);
+		_rigidbody.velocity *= Mathf.Clamp01(1f - (1f + _brakeInput * brakeStrength) * drag * Time.fixedDeltaTime);
 	}
 }
